0d3e1025bccac89a2047e1956e956459ddb25b37
Emory Soper <emorysoper@icloud.com>
2023-04-12 10:18:08 -0400

    made function

--- js/site.js
+++ js/site.js
@@ -1,11 +1,18 @@
-function displayMessage() {
-    let msg = document.getElementById("message").value;
-    //alert(msg);
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg
-        }
-    );
+function reverseString() {
+    let str = document.getElementById("inputstr").value;
+
+    if (str.length == 0) {
+        return;
+    }
+
+    let rev = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+
+    Swal.fire({
+        backdrop: false,
+        title: "APP NAME",
+        text: str + " backwards is " + rev,
+    });
 }
